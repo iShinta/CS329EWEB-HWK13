c7e51e72ffeb0e4067fcb44c09d5959019de0d49
iShinta <ho_minh_tri@yahoo.fr>
2016-11-14 08:57:06 -0600

    Added login 0.1

--- article1.php
+++ article1.php
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-  <h1>Article 1</h1>
+  <h1>Reine Priebus, the party leader, is named Chief of Staff</h1>
   <?php include 'menu.php'; ?>
   <p>President-elect Donald J. Trump chose Mr. Priebus, whose friendship with the House speaker, Paul D. Ryan, could help secure early legislative victories.</p>
 </body>
---
+++ login.php
@@ -0,0 +1,74 @@
+<?php
+function start(){
+  //Login is included at the start of each page to see if they are logged in.
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){ //Form has been posted
+    if(isset($_POST["username"])){
+      $username = $_POST["username"];
+      $password = $_POST["password"];
+
+      //TODO: Check file to see if user is authorized
+      if($username == "guest" && $password == "welcome"){
+        echo "Login Succeeded. Welcome ".$username. ".<br />";
+        setcookie("id", session_id, time()+120);
+        setcookie("timeloggedin", time(), time()+120);
+        showLogged();
+      }else{
+        echo "Login Failed.<br />Bad username or password";
+        echo "<br />You entered username: ".$username;
+        echo "<br />and Password: ".$password;
+        echo "<br /><a href=\"http://zweb.cs.utexas.edu/users/cs329e-fa16/minhtri/hwk13/index.php\"> Back to the form </a>";
+      }
+    }else if(isset($_POST["logout"])){
+      // echo "logout";
+      showLoggedOut();
+    }
+  }else{ //Not after a POST
+    if(isset($_COOKIE["id"])){ //Logged in
+      //Show article
+      showLogged();
+    }else{ //Is not in a `session`
+      // echo "Not in a session, Showing Login";
+      showLogin();
+    }
+  }
+
+  function showLogin(){ ?>
+    <p>Please Log In</p>
+    <form method="post" action="#">
+      <input type="text" name="username" />
+      <input type="text" name="password" /><br />
+      <input type="submit" name="submit" value="Submit" />
+      <input type="reset" name="reset" value="Reset" />
+    </form>
+    <p>You don't have a login? Register here: <a href="register.php">Register</a></p>
+  <?php }
+
+  function showLogged(){ //TODO: Show article
+    echo "Cookie id: ".$_COOKIE["id"];
+    echo "<br /> Time logged in: ".$_COOKIE["timeloggedin"]; ?>
+    <form method="post" action="#">
+      <input type="submit" name="logout" value="Log Out" />
+    </form>
+    <?php
+    //Form has been submitted, check if login ok
+  }
+
+  function showLoggedOut(){
+    unset($_COOKIE["id"]);
+    unset($_COOKIE["timeloggedin"]);
+    setcookie("id", '', time() - 3600);
+    setcookie("timeloggedin", '', time() - 3600);
+    echo "<p>Thank You. You are now logged out</p>";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+
+  <body>
+    <?php start(); ?>
+  </body>
+</html>
