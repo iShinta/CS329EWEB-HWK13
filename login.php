43635ae17a6097acb7850cca08ffd707725ccc95
iShinta <ho_minh_tri@yahoo.fr>
2016-11-14 15:14:45 -0600

    Login 0.4

--- login.php
+++ login.php
@@ -5,34 +5,16 @@ function start(){
     echo "Hello";
     }
   }else{ //Not after a POST
-    if(isset($_COOKIE["id"])){ //Logged in
-      //Show article
-      showLogged();
-    }else{ //Is not in a `session`
-      // echo "Not in a session, Showing Login";
-      showLogin();
-    }
+    echo "Wesh";
   }
 
   function showLogin(){ ?>
     <p>Please Log In</p>
-    <form method="post" action="#">
-      <input type="text" name="username" />
-      <input type="text" name="password" /><br />
-      <input type="submit" name="submit" value="Submit" />
-      <input type="reset" name="reset" value="Reset" />
-    </form>
-    <p>You don't have a login? Register here: <a href="register.php">Register</a></p>
+
   <?php }
 
   function showLogged(){ //TODO: Show article
-    echo "Cookie id: ".$_COOKIE["id"];
-    echo "<br /> Time logged in: ".$_COOKIE["timeloggedin"]; ?>
-    <form method="post" action="#">
-      <input type="submit" name="logout" value="Log Out" />
-    </form>
-    <?php
-    //Form has been submitted, check if login ok
+    echo "Proup";
   }
 
   function showLoggedOut(){
