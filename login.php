fb4555d22f0646d3c1d98f6e6bf72120ab6b682f
iShinta <ho_minh_tri@yahoo.fr>
2016-11-14 15:21:20 -0600

    Login 0.7

--- login.php
+++ login.php
@@ -1,50 +1,50 @@
 <?php
-function start(){
-  //Login is included at the start of each page to see if they are logged in.
-  if($_SERVER['REQUEST_METHOD'] === 'POST'){ //Form has been posted
-    if(isset($_POST["username"])){
-      $username = $_POST["username"];
-      $password = $_POST["password"];
+  function start(){
+    //Login is included at the start of each page to see if they are logged in.
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){ //Form has been posted
+      if(isset($_POST["username"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-      //TODO: Check file to see if user is authorized
-      $fh = fopen("passwd.txt", "r");
-      //Check if username is already taken
-      $userlist = Array();
-      while(!feof($fh)){
-        //Read Line
-        $line = fgets($fh);
-        $line_pieces = explode(":", $line);
-        $userlist[$line_pieces[0]] = $line_pieces[1];
+        //TODO: Check file to see if user is authorized
+        $fh = fopen("passwd.txt", "r");
+        //Check if username is already taken
+        $userlist = Array();
+        while(!feof($fh)){
+          //Read Line
+          $line = fgets($fh);
+          $line_pieces = explode(":", $line);
+          $userlist[$line_pieces[0]] = $line_pieces[1];
+        }
+        fclose($fh);
+
+        echo 'Password pour ce username: ';
+        print($userlist[$username]."<br />");
+        if(array_key_exists($username, $userlist) && $userlist[$username] == $password){
+          echo "Login Succeeded. Welcome ".$username. ".<br />";
+          setcookie("id", session_id, time()+120);
+          setcookie("timeloggedin", time(), time()+120);
+          showLogged();
+        }else{
+          echo "Login Failed.<br />Bad username or password";
+          echo "<br />You entered username: ".$username;
+          echo "<br />and Password: ".$password;
+          echo "<br /><a href=\"http://zweb.cs.utexas.edu/users/cs329e-fa16/minhtri/hwk13/index.php\"> Back to the form </a>";
+        }
+      }else if(isset($_POST["logout"])){
+        // echo "logout";
+        showLoggedOut();
       }
-      fclose($fh);
-
-      echo 'Password pour ce username';
-      print($userlist[$username]);
-      if(array_key_exists($username, $userlist) && $userlist[$username] == $password){
-        echo "Login Succeeded. Welcome ".$username. ".<br />";
-        setcookie("id", session_id, time()+120);
-        setcookie("timeloggedin", time(), time()+120);
+    }else{ //Not after a POST
+      if(isset($_COOKIE["id"])){ //Logged in
+        //Show article
         showLogged();
-      }else{
-        echo "Login Failed.<br />Bad username or password";
-        echo "<br />You entered username: ".$username;
-        echo "<br />and Password: ".$password;
-        echo "<br /><a href=\"http://zweb.cs.utexas.edu/users/cs329e-fa16/minhtri/hwk13/index.php\"> Back to the form </a>";
+      }else{ //Is not in a `session`
+        // echo "Not in a session, Showing Login";
+        showLogin();
       }
-    }else if(isset($_POST["logout"])){
-      // echo "logout";
-      showLoggedOut();
-    }
-  }else{ //Not after a POST
-    if(isset($_COOKIE["id"])){ //Logged in
-      //Show article
-      showLogged();
-    }else{ //Is not in a `session`
-      // echo "Not in a session, Showing Login";
-      showLogin();
     }
   }
-}
 
   function showLogin(){ ?>
     <p>Please Log In</p>
