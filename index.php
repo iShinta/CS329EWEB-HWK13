bce8ac2dea580b9bfe9ab04d404a9fba8ff0ae30
iShinta <ho_minh_tri@yahoo.fr>
2016-11-13 23:08:32 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Newspaper</title>
+</head>
+
+<body>
+  <ul>
+    <li><a href="article1.php">Article 1</a></li>
+    <li><a href="article2.php">Article 2</a></li>
+    <li><a href="article3.php">Article 3</a></li>
+    <li><a href="article4.php">Article 4</a></li>
+    <li><a href="article5.php">Article 5</a></li>
+</body>
+</html>
