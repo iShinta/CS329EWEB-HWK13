33f76bdd0295fd0908f05ea80ca155ada597a311
iShinta <ho_minh_tri@yahoo.fr>
2016-11-14 17:15:28 -0600

    Style 0.5

--- article1.php
+++ article1.php
@@ -11,7 +11,13 @@
   <div class="container">
     <h1>Reine Priebus, the party leader, is named Chief of Staff</h1>
     <?php include 'menu.php'; ?>
+    <?php if(isset($_COOKIE["id"])){
+
+    ?>
     <p>President-elect Donald J. Trump chose Mr. Priebus, whose friendship with the House speaker, Paul D. Ryan, could help secure early legislative victories.</p>
+    <?php }else{
+      echo "You need to log in to read this article."
+    }?>
   </div>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -76,15 +76,16 @@
   }
 ?>
 
-
-<h2><Menu></h2>
-<ul>
-  <li><a href="article1.php">Reine Priebus, the party leader, is named Chief of Staff</a></li>
-  <li><a href="article2.php">Trump on Abortion and Immigration, and back on Twitter</a></li>
-  <li><a href="article3.php">Syrian Rebels Brace for a Trump Cutoff, but See Some Upside</a></li>
-  <li><a href="article4.php">Teslas in Trailer Parks: California’s Housing Squeeze</a></li>
-  <li><a href="article5.php">‘DNA Is Not Destiny’ With Heart Disease, New Research Says</a></li>
-</ul>
+<div class="menu">
+  <h2><Menu></h2>
+  <ul>
+    <li><a href="article1.php">Reine Priebus, the party leader, is named Chief of Staff</a></li>
+    <li><a href="article2.php">Trump on Abortion and Immigration, and back on Twitter</a></li>
+    <li><a href="article3.php">Syrian Rebels Brace for a Trump Cutoff, but See Some Upside</a></li>
+    <li><a href="article4.php">Teslas in Trailer Parks: California’s Housing Squeeze</a></li>
+    <li><a href="article5.php">‘DNA Is Not Destiny’ With Heart Disease, New Research Says</a></li>
+  </ul>
+</div>
 <div class="login">
   <?php start(); ?>
 </div>
