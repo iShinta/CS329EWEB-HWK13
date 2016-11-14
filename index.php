daefdd28ec8dac2b12dde10fede7614593d1061e
iShinta <ho_minh_tri@yahoo.fr>
2016-11-14 08:06:45 -0600

    include Menu 0.1

--- index.php
+++ index.php
@@ -7,12 +7,6 @@
 
 <body>
   <h1>Newspaper</h1>
-  <ul>
-    <li><a href="article1.php">Article 1</a></li>
-    <li><a href="article2.php">Article 2</a></li>
-    <li><a href="article3.php">Article 3</a></li>
-    <li><a href="article4.php">Article 4</a></li>
-    <li><a href="article5.php">Article 5</a></li>
-  </ul>
+  <?php include(menu.php); ?>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<ul>
+  <li><a href="article1.php">Article 1</a></li>
+  <li><a href="article2.php">Article 2</a></li>
+  <li><a href="article3.php">Article 3</a></li>
+  <li><a href="article4.php">Article 4</a></li>
+  <li><a href="article5.php">Article 5</a></li>
+</ul>
