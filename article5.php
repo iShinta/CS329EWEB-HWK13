63ac7a956740974f041fc4e2433e8c25c1672ce4
iShinta <ho_minh_tri@yahoo.fr>
2016-11-14 19:38:52 -0600

    Content 0.3

--- article5.php
+++ article5.php
@@ -14,7 +14,7 @@
     <?php if(isset($_COOKIE["id"])){
 
     ?>
-    <h2>Reine Priebus, the party leader, is named Chief of Staff</h2>
+    <h2>‘DNA Is Not Destiny’ With Heart Disease, New Research Says</h2>
     <p>Even people who are at the worst risk can benefit by not smoking, exercising moderately and eating a healthy diet, a new study says.</p>
     <?php }else{
       echo "<p>You need to log in to read this article.</p>";
